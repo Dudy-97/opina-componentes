6771b1e460566404cd535c85c963f25baf263ff4
Dudy-97 <78860255+Dudy-97@users.noreply.github.com>
2021-02-17 12:05:44 +0100

    Probando Egit

---
+++ opina-componentes/src/main/java/es/urjc/dad/practica/OpinaComponentesApplication.java
@@ -0,0 +1,13 @@
+package es.urjc.dad.practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpinaComponentesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpinaComponentesApplication.class, args);
+	}
+
+}
---
+++ opina-componentes/src/test/java/es/urjc/dad/practica/OpinaComponentesApplicationTests.java
@@ -0,0 +1,13 @@
+package es.urjc.dad.practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpinaComponentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
