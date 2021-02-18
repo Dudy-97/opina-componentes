83dae1ab3dd91ee80474848a004ea16de2c129af
qvicfri1 <60407168+qvicfri1@users.noreply.github.com>
2021-02-18 13:00:32 +0100

    Creacion de html 1
    Se han creado y empezado a rellenear algunas de las primeras páginas.
    
    Co-Authored-By: Dudy-97 <78860255+Dudy-97@users.noreply.github.com>

--- opina-componentes/src/main/java/es/urjc/dad/practica/LogController.java
+++ opina-componentes/src/main/java/es/urjc/dad/practica/LogController.java
@@ -1,11 +1,22 @@
 package es.urjc.dad.practica;
 
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 
-import ch.qos.logback.classic.Logger;
 
-@Controller
+//@Controller
 public class LogController {
 
-	private Logger log = LoggerFactory.getLogger(LogController.class);
+    /*private Logger log = LoggerFactory.getLogger(LogController.class);
+
+    @GetMapping("/log_page")
+    public String page(Model model) {
+        log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("A INFO Message");
+        log.warn("A WARN Message");
+        log.error("An ERROR Message");
+
+        return "log-page";
+    }*/
 }
