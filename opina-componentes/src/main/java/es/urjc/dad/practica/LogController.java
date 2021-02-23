a4d21dbe27435b463525712749417ce60d6b503f
qvicfri1 <60407168+qvicfri1@users.noreply.github.com>
2021-02-23 14:51:32 +0100

    Clase dia 23
    Avance sobre las repository y clase Producto, arreglo de algunas paginas html
    
    Co-Authored-By: Dudy-97 <78860255+Dudy-97@users.noreply.github.com>

--- opina-componentes/src/main/java/es/urjc/dad/practica/LogController.java
+++ opina-componentes/src/main/java/es/urjc/dad/practica/LogController.java
@@ -1,16 +1,19 @@
 package es.urjc.dad.practica;
 
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-
-//@Controller
+@Controller
 public class LogController {
 
-    /*private Logger log = LoggerFactory.getLogger(LogController.class);
+    private Logger log = LoggerFactory.getLogger(LogController.class);
 
     @GetMapping("/log_page")
     public String page(Model model) {
+    	
         log.trace("A TRACE Message");
         log.debug("A DEBUG Message");
         log.info("A INFO Message");
@@ -18,5 +21,5 @@ public class LogController {
         log.error("An ERROR Message");
 
         return "log-page";
-    }*/
+    }
 }
