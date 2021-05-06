d870ef0e8d33b611a9438a1446066cb3bfbb9df8
qvicfri1 <60407168+qvicfri1@users.noreply.github.com>
2021-05-06 12:50:27 +0200

    Creacion del Docker y Hazelcast
    Se ha creado el archivo Docker y añadido la opcion Hazelcast
    
    Co-Authored-By: Dudy-97 <78860255+Dudy-97@users.noreply.github.com>

--- opina-componentes/src/main/java/es/urjc/dad/practica/OpinaComponentesApplication.java
+++ opina-componentes/src/main/java/es/urjc/dad/practica/OpinaComponentesApplication.java
@@ -3,11 +3,34 @@ package es.urjc.dad.practica;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//Adicion de cache-distribuida
+import java.util.Collections;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig; 
+
 @SpringBootApplication
+@EnableHazelcastHttpSession
 public class OpinaComponentesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpinaComponentesApplication.class, args);
 	}
 
+	@Bean
+    public Config config() {
+
+        Config config = new Config();
+
+        JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+
+        //Multicast permite encontrar a otros usuarios con multicast de Hazelcast
+        joinConfig.getMulticastConfig().setEnabled(true);
+
+        return config;
+    }
+	
 }
